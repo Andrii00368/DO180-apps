2ae7ed47e62bc572a27d8f33f27976fbef9b9d08
Student User <student@workstation.lab.example.com>
2020-02-11 12:42:01 +0000

    3rd line added

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "--== The Change is on the Air ==--" . PHP_VERSION . "\n";
+print "~~~~~~~~~~ another lIne ~~~~~~~~" . PHP_VERSION . "\n";
 ?>
